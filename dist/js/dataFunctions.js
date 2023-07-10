fd76bfe0a4fa46515d54d19c854af8dccad0d312
oluwafunmike <oluwatuyiolufunke@gmail.com>
2023-07-11 00:32:42 +0100

    dataFunctions

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -33,7 +33,7 @@ export const getWeatherFromCoords = async (locationObj) => {
     };
     try {
       const weatherStream = await fetch("./.netlify/functions/get_weather", {
-        method: "POST",
+        method: "GET",
         body: JSON.stringify(urlDataObj)
       });
       const weatherJson = await weatherStream.json();
