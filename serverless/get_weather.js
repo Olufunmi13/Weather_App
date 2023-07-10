4102caf853fe6ac87b53b03c011fbd40f9265135
oluwafunmike <oluwatuyiolufunke@gmail.com>
2023-07-10 23:50:00 +0100

    corrected

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -1,10 +1,10 @@
-const fetch = require("node_fetch")
+const fetch = require("node-fetch");
 
 const {WEATHER_API_KEY} = process.env;
 
-exports.handler = async(event,context) =>{
+exports.handler = async(event, context) =>{
     const params = JSON.parse(event.body);
-    const {lat,lon,unit} = params;
+    const {lat,lon,units} = params;
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${WEATHER_API_KEY}`;
     try {
         const weatherStream = await fetch(url);
